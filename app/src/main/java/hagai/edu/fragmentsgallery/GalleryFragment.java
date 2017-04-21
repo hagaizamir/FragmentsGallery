5176b9e345c5df5aae56964709f4d8be8b6a4901
Hagai Zamir <hagai34@gmail.com>
2017-04-21 10:09:57 +0300

    Initial commit

---
+++ app/src/main/java/hagai/edu/fragmentsgallery/GalleryFragment.java
@@ -0,0 +1,37 @@
+package hagai.edu.fragmentsgallery;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GalleryFragment extends Fragment {
+    ImageView ivGallery;
+    Button btnLeft,btnRight;
+    int [] images = {R.drawable.star,R.drawable.activity_man,R.drawable.time};
+    int imageIndex = 0;
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
